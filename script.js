98f47ae7dab9e4894ad877861b5bc051aa883ab1
liewmang <73412830+liewmang@users.noreply.github.com>
2021-05-29 18:25:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function compute()
         document.getElementById("principal").focus();
     }
     else {
-        document.getElementById("result").innerHTML="If you deposit "+<mark>principal</mark>+",\<br\>at an interest rate of "+<mark>rate</mark>+"%\<br\>You will receive an amount of "+<mark>interest</mark>+",\<br\>in the year "+<mark>year</mark>+"\<br\>";
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
     }
 }
 function updateRate() 
